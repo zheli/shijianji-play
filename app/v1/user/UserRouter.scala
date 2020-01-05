41ca4981791810136646992709bb9c3a687c2a74
Zhe Li <linuxcity.jn@gmail.com>
2020-01-05 12:52:17 +0100

    Add UserRouter.

---
+++ app/v1/user/UserRouter.scala
@@ -0,0 +1,20 @@
+package v1.user
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+import play.api.routing.sird._
+
+class UserRouter @Inject() (controller: UserController) extends SimpleRouter {
+  val prefix = "/v1/Users"
+
+  def link(id: UserId): String = {
+    import io.lemonlabs.uri.dsl._
+    val url = prefix / id.toString
+    url.toString
+  }
+  override def routes: Routes = {
+    case GET(p"/") =>
+      controller.index
+  }
+}
