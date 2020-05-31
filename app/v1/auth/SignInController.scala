d3edfa2b3369eefe81b7d7558ad3c30df2e5af81
Zhe Li <linuxcity.jn@gmail.com>
2020-06-01 00:30:28 +0200

    Fix sign in endpoint

--- app/v1/auth/SignInController.scala
+++ app/v1/auth/SignInController.scala
@@ -5,16 +5,18 @@ import java.time.Clock
 import com.mohiva.play.silhouette.api.exceptions.ProviderException
 import com.mohiva.play.silhouette.api.util.Credentials
 import com.mohiva.play.silhouette.api.{LoginEvent, LoginInfo, Silhouette}
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
 import com.mohiva.play.silhouette.impl.exceptions.IdentityNotFoundException
 import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import formatters.json.CredentialsFormat
 import javax.inject.Inject
 import models.User
 import net.ceedubs.ficus.Ficus._
 import org.joda.time.DateTime
 import play.api.Configuration
 import play.api.i18n.Messages
-import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, ControllerComponents, Request, RequestHeader, Result}
+import play.api.libs.json.{JsString, Json}
+import play.api.mvc.{Action, AnyContent, ControllerComponents, Cookie, Request, RequestHeader, Result}
 import services.UserService
 import shared.controllers.ApiController
 import utils.DefaultEnv
@@ -23,51 +25,67 @@ import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{ExecutionContext, Future}
 
 // Inspired by play-silhouette-seed
-class SignInController @Inject() (
+class SignInController @Inject()(
   val controllerComponents: ControllerComponents,
   silhouette: Silhouette[DefaultEnv],
   userService: UserService,
   credentialsProvider: CredentialsProvider,
   configuration: Configuration,
   clock: Clock
-)(implicit ex:ExecutionContext) extends ApiController {
+)(implicit ex: ExecutionContext)
+    extends ApiController {
+
+  implicit val credentialsFormat = CredentialsFormat.emailAsIdentifierFormat
+
   /**
-   * Sign in a user.
-   *
-   * @return A Play result.
-   */
-  def signIn: Action[AnyContent] = silhouette.UnsecuredAction.async { implicit request: Request[AnyContent] =>
-    SignInForm.form.bindFromRequest.fold(
-      form => Future.successful(BadRequest(
-        ApiResponse("auth.signIn.form.invalid", Messages("invalid.form"), form.errors)
-      )),
-      data => {
-        credentialsProvider.authenticate(Credentials(data.email, data.password)).flatMap { loginInfo: LoginInfo =>
-          userService.retrieve(loginInfo).flatMap {
-//            case Some(user) if !user.registration.activated =>
-//              handleInactiveUser(user)
-            case Some(user) =>
-              handleActiveUser(user, loginInfo, data.rememberMe)
-            case None =>
-              Future.failed(new IdentityNotFoundException("Couldn't find user"))
-          }
-        }.recover {
-          case _: ProviderException =>
-            BadRequest(ApiResponse("auth.signIn.credentials", Messages("auth.invalid.credentials")))
+    * Sign in a user.
+    *
+    * @return A Play result.
+    */
+  def signIn = silhouette.UnsecuredAction.async(parse.json[Credentials]) { implicit request =>
+    //  def signIn = silhouette.UnsecuredAction.async { implicit request =>
+    logger.debug(s"Request: $request")
+    val credentials = Credentials(request.body.identifier, request.body.password)
+    credentialsProvider
+      .authenticate(credentials)
+      .flatMap { loginInfo =>
+        userService.retrieve(loginInfo).flatMap {
+          case Some(user) =>
+            silhouette.env.authenticatorService
+              .create(loginInfo)
+              .map {
+                case authenticator => authenticator
+              }
+              .flatMap { authenticator: CookieAuthenticator =>
+                //TODO enable eventBus
+                //silhouette.env.eventBus.publish(LoginEvent(user, request))
+                silhouette.env.authenticatorService
+                  .init(authenticator)
+                  .flatMap { token: Cookie =>
+                    silhouette.env.authenticatorService
+                      .embed(token, Ok(""))
+                  }
+              }
+
+          case None =>
+            Future.failed(new IdentityNotFoundException("Couldn't find user"))
         }
       }
-    )
+      .recover {
+        case _: ProviderException =>
+          BadRequest
+      }
   }
 
   /**
-   * Handles the active user.
-   *
-   * @param user       The active user.
-   * @param loginInfo  The login info for the current authentication.
-   * @param rememberMe True if the cookie should be a persistent cookie, false otherwise.
-   * @param request    The current request header.
-   * @return A Play result.
-   */
+    * Handles the active user.
+    *
+    * @param user       The active user.
+    * @param loginInfo  The login info for the current authentication.
+    * @param rememberMe True if the cookie should be a persistent cookie, false otherwise.
+    * @param request    The current request header.
+    * @return A Play result.
+    */
   private def handleActiveUser(
     user: User,
     loginInfo: LoginInfo,
@@ -75,30 +93,33 @@ class SignInController @Inject() (
   )(implicit request: RequestHeader): Future[Result] = {
     import models.User._
 
-    silhouette.env.authenticatorService.create(loginInfo)
+    silhouette.env.authenticatorService
+      .create(loginInfo)
       .map(configureAuthenticator(rememberMe, _))
       .flatMap { authenticator =>
         silhouette.env.eventBus.publish(LoginEvent(user, request))
         silhouette.env.authenticatorService.init(authenticator).flatMap { cookie =>
           silhouette.env.authenticatorService.embed(
             cookie,
-            Ok(ApiResponse(
-              "auth.signIn.successful",
-              Messages("auth.signed.in"),
-              Json.toJson(user)
-            ))
+            Ok(
+              ApiResponse(
+                "auth.signIn.successful",
+                Messages("auth.signed.in"),
+                Json.toJson(user)
+              )
+            )
           )
         }
       }
   }
 
   /**
-   * Changes the default authenticator config if the remember me flag was activated during sign-in.
-   *
-   * @param rememberMe    True if the cookie should be a persistent cookie, false otherwise.
-   * @param authenticator The authenticator instance.
-   * @return The changed authenticator if the remember me flag was activated, otherwise the unchanged authenticator.
-   */
+    * Changes the default authenticator config if the remember me flag was activated during sign-in.
+    *
+    * @param rememberMe    True if the cookie should be a persistent cookie, false otherwise.
+    * @param authenticator The authenticator instance.
+    * @return The changed authenticator if the remember me flag was activated, otherwise the unchanged authenticator.
+    */
   private def configureAuthenticator(rememberMe: Boolean, authenticator: DefaultEnv#A): DefaultEnv#A = {
     if (rememberMe) {
       val c = configuration.underlying
--- test/v1/user/AuthSpec.scala
+++ test/v1/user/AuthSpec.scala
@@ -64,18 +64,32 @@ class AuthSpec extends PlaySpec with BeforeAndAfter with GuiceOneAppPerSuite wit
       val requestBody = Json.obj("email" -> "ha.com", "password" -> "test123")
       val request = withAcceptJsonHeader(FakeRequest(POST, "/v1/auth/sign-up")).withJsonBody(requestBody)
       val requestResult: Future[Result] = route(app, request).get
-      contentAsJson(requestResult) mustBe None
       (contentAsJson(requestResult) \ "details").head.as[Map[String, String]].get("key") mustEqual Some("email")
       cookies(requestResult) mustBe empty
 
       val requestBody2 = Json.obj("email" -> "", "password" -> "test123")
-      val request2 = withAcceptJsonHeader(FakeRequest(POST, "/v1/auth/sign-up")).withJsonBody(requestBody)
-      val requestResult2: Future[Result] = route(app, request).get
+      val request2 = withAcceptJsonHeader(FakeRequest(POST, "/v1/auth/sign-up")).withJsonBody(requestBody2)
+      val requestResult2: Future[Result] = route(app, request2).get
       (contentAsJson(requestResult2) \ "details").head.as[Map[String, String]].get("key") mustEqual Some("email")
       cookies(requestResult2) mustBe empty
 
       val users = await(usersDao.list())
       users.length mustBe 0
+    }
+
+    "sign in when credentials is correct" in {
+      val usersDao = inject[UsersDAOImpl]
+      val userEmail = "ha@ha.com"
+      val requestBody = Json.obj("email" -> userEmail, "password" -> "test123")
+      val request1 = withAcceptJsonHeader(FakeRequest(POST, "/v1/auth/sign-up")).withJsonBody(requestBody)
+      await(route(app, request1).get)
+      await(usersDao.list())
+
+      val request2 = withAcceptJsonHeader(FakeRequest(POST, "/v1/auth/sign-in")).withJsonBody(requestBody)
+      val response = route(app, request2).get
+      status(response) mustBe OK
+      (contentAsString(response)) mustBe ""
+      cookies(response) must not be empty
     }
   }
 }
