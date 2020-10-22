880a2eeb64224f044efb54ab0c3279f9a54fffdb
Zhe Li <linuxcity.jn@gmail.com>
2020-10-22 23:55:28 +0200

    Add some Scaladocs

--- app/utils/MyPostgresProfile.scala
+++ app/utils/MyPostgresProfile.scala
@@ -2,27 +2,24 @@ package utils
 
 import com.github.tminglei.slickpg._
 
-trait MyPostgresProfile extends ExPostgresProfile
-  with PgArraySupport
-  with PgDate2Support
-  with PgPlayJsonSupport
-  with PgNetSupport
-  with PgLTreeSupport
-  with PgRangeSupport
-  with PgHStoreSupport
-  with PgSearchSupport {
+/**
+  * Enable Postgres specific features
+  */
+trait MyPostgresProfile
+    extends ExPostgresProfile
+    with PgArraySupport
+    with PgDate2Support
+    with PgPlayJsonSupport
+    with PgNetSupport
+    with PgLTreeSupport
+    with PgRangeSupport
+    with PgHStoreSupport
+    with PgSearchSupport {
 
   override val pgjson = "jsonb"
 
-  override val api = new API with ArrayImplicits
-    with DateTimeImplicits
-    with PlayJsonImplicits
-    with NetImplicits
-    with LTreeImplicits
-    with RangeImplicits
-    with HStoreImplicits
-    with SearchImplicits
-    with SearchAssistants {}
+  override val api = new API with ArrayImplicits with DateTimeImplicits with PlayJsonImplicits with NetImplicits with LTreeImplicits
+  with RangeImplicits with HStoreImplicits with SearchImplicits with SearchAssistants {}
 }
 
 object MyPostgresProfile extends MyPostgresProfile
--- app/utils/RequestMarkerContext.scala
+++ app/utils/RequestMarkerContext.scala
@@ -4,12 +4,15 @@ import net.logstash.logback.marker.LogstashMarker
 import play.api.MarkerContext
 import play.api.mvc.RequestHeader
 
+/**
+  * Add extra request markers in logs
+  */
 trait RequestMarkerContext {
   import net.logstash.logback.marker.Markers
 
   private def marker(tuple: (String, Any)) = Markers.append(tuple._1, tuple._2)
 
-  private implicit class RichLogstashMarker(marker1: LogstashMarker) {
+  implicit private class RichLogstashMarker(marker1: LogstashMarker) {
     def &&(marker2: LogstashMarker): LogstashMarker = marker1.and(marker2)
   }
 
